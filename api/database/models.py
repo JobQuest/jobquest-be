2539578332853df391ca96eda71836c27421ae82
George Soderholm <georgesoderholm@gmail.com>
2021-02-21 21:50:53 -0700

    Update models with different configuration of the ForeignKey

--- api/database/models.py
+++ api/database/models.py
@@ -1,9 +1,7 @@
 import bleach
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, ForeignKey
+from datetime import datetime
 from api import db
-# from api.database.submodels import *
-# from submodels.user_quest.py import *
-# from submodels.quest_encounter.py import *
 
 
 class User(db.Model):
@@ -18,8 +16,12 @@ class User(db.Model):
     username = Column(String(80), unique=True, nullable=False)
     # unique email
     email = Column(String(100), unique=True, nullable=False)
+    # time_stamp
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # relationships
+    # user_quests = db.relationship('UserQuest', backref='user_quest.id', lazy='dynamic')
 
-    def __init__(self, username, email, user_id=None):
+    def __init__(self, username, email, timestamp, user_id=None):
         if username is not None:
             username = bleach.clean(username).strip()
             if username == '':
@@ -30,8 +32,14 @@ class User(db.Model):
             if email == '':
                 email = None
 
+        if timestamp is not None:
+            timestamp = bleach.clean(timestamp).strip()
+            if timestamp == '':
+                timestamp = None
+
         self.username = username
         self.email = email
+        self.timestamp = timestamp
         if user_id is not None:
             self.id = user_id
 
@@ -75,37 +83,37 @@ class Quest(db.Model):
     encounter_req = Column(Integer, nullable=False)
     # type
     type = Column(String(80), nullable=False)
+    # time_stamp
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # relationships
+    # quests_encounters = db.relationship('QuestEncounter', backref='quest_encounter', lazy='dynamic')
+    # user_quests = db.relationship('UserQuest', backref='user_quest', lazy='dynamic')
 
-    def __init__(self, name, xp, encounter_req, type):
+    def __init__(self, name, xp, encounter_req, type, timestamp):
         if name is not None:
             name = bleach.clean(name).strip()
             if name == '':
                 name = None
-
-        if xp is not None:
-            xp = bleach.clean(xp).strip()
-            if xp == '':
-                xp = None
-
-        if encounter_req is not None:
-            encounter_req = bleach.clean(encounter_req).strip()
-            if encounter_req == '':
-                encounter_req = None
 
         if type is not None:
             type = bleach.clean(type).strip()
             if type == '':
                 type = None
 
+        if timestamp is not None:
+            timestamp = bleach.clean(timestamp).strip()
+            if timestamp == '':
+                timestamp = None
+
         self.name = name
         self.xp = xp
         self.encounter_req = encounter_req
         self.type = type
+        self.timestamp = timestamp
 
     def insert(self):
         """
         inserts a new model into a database
-        the model must have a unique username
         the model must have a unique id or null id
         """
         db.session.add(self)
@@ -135,45 +143,30 @@ class UserQuest(db.Model):
     # Auto-incrementing, unique primary key
     id = Column(Integer, primary_key=True)
     # quest_id, foreign_key to the quest table
-    quest_id = Column(Integer, foreign_key='quest.id', nullable=False)
+    quest_id = Column(Integer, ForeignKey('quests.id'), nullable=False)
     # user_id, foreign_key to the user table
-    user_id = Column(Integer, foreign_key='user.id', nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     # completion_status
     completion_status = Column(Boolean, nullable=False)
     # progress
-    progress = Column(Integer, nullable=False)
+    progress = Column(Integer, default=1, nullable=False)
+    # time_stamp
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # relationships
+    # users = db.relationship('User', backref='user', lazy='dynamic')
+    # quests = db.relationship('Quest', backref='quest', lazy='dynamic')
 
-
-    def __init__(self, quest_id, user_id, completion_status, progress):
-        if quest_id is not None:
-            quest_id = bleach.clean(quest_id).strip()
-            if quest_id == '':
-                quest_id = None
-
-        if user_id is not None:
-            user_id = bleach.clean(user_id).strip()
-            if user_id == '':
-                user_id = None
-
-        if completion_status is not None:
-            completion_status = bleach.clean(completion_status).strip()
-            if completion_status == '':
-                completion_status = None
-
-        if progress is not None:
-            progress = bleach.clean(progress).strip()
-            if progress == '':
-                progress = None
+    def __init__(self, quest_id, user_id, completion_status, progress, timestamp):
 
         self.quest_id = quest_id
         self.user_id = user_id
         self.completion_status = completion_status
         self.progress = progress
+        self.timestamp = timestamp
 
     def insert(self):
         """
         inserts a new model into a database
-        the model must have a unique username
         the model must have a unique id or null id
         """
         db.session.add(self)
@@ -208,8 +201,14 @@ class Encounter(db.Model):
     type = Column(String(80), nullable=False)
     # monster_image
     monster_image = Column(String(3000), nullable=False)
+    # quest_id
+    quest_id = Column(Integer, ForeignKey('quests.id'), nullable=False)
+    # time_stamp
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # relationships
+    # actions = db.relationship('Action', backref='action', lazy='dynamic')
 
-    def __init__(self, description, type, monster_image):
+    def __init__(self, description, type, monster_image, quest_id, timestamp):
         if description is not None:
             description = bleach.clean(description).strip()
             if description == '':
@@ -228,70 +227,12 @@ class Encounter(db.Model):
         self.description = description
         self.type = type
         self.monster_image = monster_image
-
-    def insert(self):
-        """
-        inserts a new model into a database
-        the model must have a unique username
-        the model must have a unique id or null id
-        """
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        updates a new model into a database
-        the model must exist in the database
-        """
-        db.session.commit()
-
-    def delete(self):
-        """
-        deletes model from database
-        the model must exist in the database
-        """
-        db.session.delete(self)
-        db.session.commit()
-
-class QuestEncounter(db.Model):
-    """
-    QuestEncounter Model
-    """
-    __tablename__ = 'quest_encounters'
-
-    # Auto-incrementing, unique primary key
-    id = Column(Integer, primary_key=True)
-    # quest_id, foreign_key to the quest table
-    quest_id = Column(Integer, foreign_key='quest.id', nullable=False)
-    # type
-    type = Column(String(80), nullable=False)
-    # encounter_id, foreign_key to the encounter table
-    encounter_id = Column(Integer, foreign_key='encounter.id', nullable=False)
-
-    def __init__(self, quest_id, type, encounter_id):
-        if quest_id is not None:
-            quest_id = bleach.clean(quest_id).strip()
-            if quest_id == '':
-                quest_id = None
-
-        if type is not None:
-            type = bleach.clean(type).strip()
-            if type == '':
-                type = None
-
-        if encounter_id is not None:
-            encounter_id = bleach.clean(encounter_id).strip()
-            if encounter_id == '':
-                encounter_id = None
-
         self.quest_id = quest_id
-        self.type = type
-        self.encounter_id = encounter_id
+        self.timestamp = timestamp
 
     def insert(self):
         """
         inserts a new model into a database
-        the model must have a unique username
         the model must have a unique id or null id
         """
         db.session.add(self)
@@ -311,6 +252,7 @@ class QuestEncounter(db.Model):
         """
         db.session.delete(self)
         db.session.commit()
+
 
 class Action(db.Model):
     """
@@ -321,15 +263,17 @@ class Action(db.Model):
     # Auto-incrementing, unique primary key
     id = Column(Integer, primary_key=True)
     # encounter_id, foreign_key to the quest table
-    encounter_id = Column(Integer, foreign_key='encounter.id', nullable=False)
+    encounter_id = Column(Integer, ForeignKey('encounters.id'), nullable=False)
     # type
     type = Column(String(80), nullable=False)
     # title
     title = Column(String(80), nullable=False)
     # description
     description = Column(String(1000), nullable=False)
+    # time_stamp
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
 
-    def __init__(self, title, description, type, encounter_id):
+    def __init__(self, encounter_id, title, description, type, timestamp):
         if title is not None:
             title = bleach.clean(title).strip()
             if title == '':
@@ -345,15 +289,11 @@ class Action(db.Model):
             if type == '':
                 type = None
 
-        if encounter_id is not None:
-            encounter_id = bleach.clean(encounter_id).strip()
-            if encounter_id == '':
-                encounter_id = None
-
         self.title = title
         self.description = description
         self.type = type
         self.encounter_id = encounter_id
+        self.timestamp = timestamp
 
     def insert(self):
         """
