ef2046de3dbfd231fd0ae30c988a1569fa4d9c18
George Soderholm <georgesoderholm@gmail.com>
2021-02-22 18:56:23 -0700

    Update models to accurately reflect changes in the schema
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    Co-authored-by: Carson Jardine 
    <65981543+carson-jardine@users.noreply.github.com>
    Co-authored-by: Curtis Bartell 
    <60277914+c-bartell@users.noreply.github.com>

--- api/database/models.py
+++ api/database/models.py
@@ -11,17 +11,21 @@ class User(db.Model):
     __tablename__ = 'users'
 
     # Auto-incrementing, unique primary key
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     # unique username
     username = Column(String(80), unique=True, nullable=False)
     # unique email
     email = Column(String(100), unique=True, nullable=False)
-    # time_stamp
-    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # xp
+    xp = Column(Integer, nullable=False)
+    # created_at
+    created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     # relationships
-    # user_quests = db.relationship('UserQuest', backref='user_quest.id', lazy='dynamic')
+    user_quests = db.relationship('UserQuest', backref='user_quest.id', lazy='dynamic')
 
-    def __init__(self, username, email, timestamp, user_id=None):
+    def __init__(self, username, email, xp, user_id=None):
         if username is not None:
             username = bleach.clean(username).strip()
             if username == '':
@@ -32,14 +36,12 @@ class User(db.Model):
             if email == '':
                 email = None
 
-        if timestamp is not None:
-            timestamp = bleach.clean(timestamp).strip()
-            if timestamp == '':
-                timestamp = None
-
         self.username = username
         self.email = email
-        self.timestamp = timestamp
+        self.xp = xp
+        # self.created_at = created_at
+        # self.updated_at = updated_at
+
         if user_id is not None:
             self.id = user_id
 
@@ -74,7 +76,7 @@ class Quest(db.Model):
     __tablename__ = 'quests'
 
     # Auto-incrementing, unique primary key
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     # unique name
     name = Column(String(80), nullable=False)
     # xp
@@ -83,13 +85,16 @@ class Quest(db.Model):
     encounter_req = Column(Integer, nullable=False)
     # type
     type = Column(String(80), nullable=False)
-    # time_stamp
-    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # level
+    level = Column(Integer, nullable=False)
+    # created_at
+    created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     # relationships
-    # quests_encounters = db.relationship('QuestEncounter', backref='quest_encounter', lazy='dynamic')
-    # user_quests = db.relationship('UserQuest', backref='user_quest', lazy='dynamic')
+    user_quests = db.relationship('UserQuest', backref='user_quest', lazy='dynamic')
 
-    def __init__(self, name, xp, encounter_req, type, timestamp):
+    def __init__(self, name, xp, encounter_req, type, level):
         if name is not None:
             name = bleach.clean(name).strip()
             if name == '':
@@ -100,39 +105,13 @@ class Quest(db.Model):
             if type == '':
                 type = None
 
-        if timestamp is not None:
-            timestamp = bleach.clean(timestamp).strip()
-            if timestamp == '':
-                timestamp = None
-
         self.name = name
         self.xp = xp
         self.encounter_req = encounter_req
         self.type = type
-        self.timestamp = timestamp
+        self.level = level
 
-    def insert(self):
-        """
-        inserts a new model into a database
-        the model must have a unique id or null id
-        """
-        db.session.add(self)
-        db.session.commit()
 
-    def update(self):
-        """
-        updates a new model into a database
-        the model must exist in the database
-        """
-        db.session.commit()
-
-    def delete(self):
-        """
-        deletes model from database
-        the model must exist in the database
-        """
-        db.session.delete(self)
-        db.session.commit()
 
 class UserQuest(db.Model):
     """
@@ -141,50 +120,36 @@ class UserQuest(db.Model):
     __tablename__ = 'user_quests'
 
     # Auto-incrementing, unique primary key
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     # quest_id, foreign_key to the quest table
-    quest_id = Column(Integer, ForeignKey('quests.id'), nullable=False)
+    quest_id = Column(Integer, ForeignKey('quests.id'), primary_key=True)
     # user_id, foreign_key to the user table
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
     # completion_status
     completion_status = Column(Boolean, nullable=False)
     # progress
     progress = Column(Integer, default=1, nullable=False)
-    # time_stamp
-    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    # created_at
+    created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     # relationships
-    # users = db.relationship('User', backref='user', lazy='dynamic')
-    # quests = db.relationship('Quest', backref='quest', lazy='dynamic')
+    users = db.relationship('User', backref='user')
+    quests = db.relationship('Quest', backref='quest')
 
-    def __init__(self, quest_id, user_id, completion_status, progress, timestamp):
+    def __init__(self, quest_id, user_id, completion_status, progress):
 
         self.quest_id = quest_id
         self.user_id = user_id
         self.completion_status = completion_status
         self.progress = progress
-        self.timestamp = timestamp
 
-    def insert(self):
-        """
-        inserts a new model into a database
-        the model must have a unique id or null id
-        """
-        db.session.add(self)
-        db.session.commit()
 
     def update(self):
         """
         updates a new model into a database
         the model must exist in the database
         """
-        db.session.commit()
-
-    def delete(self):
-        """
-        deletes model from database
-        the model must exist in the database
-        """
-        db.session.delete(self)
         db.session.commit()
 
 class Encounter(db.Model):
@@ -194,63 +159,36 @@ class Encounter(db.Model):
     __tablename__ = 'encounters'
 
     # Auto-incrementing, unique primary key
-    id = Column(Integer, primary_key=True)
-    # description
-    description = Column(String(1000), nullable=False)
-    # type
-    type = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     # monster_image
     monster_image = Column(String(3000), nullable=False)
     # quest_id
-    quest_id = Column(Integer, ForeignKey('quests.id'), nullable=False)
-    # time_stamp
-    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    quest_id = Column(Integer, ForeignKey('quests.id'), index=True, nullable=False)
+    # progress
+    progress = Column(Integer, default=1, nullable=False)
+    # created_at
+    created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     # relationships
-    # actions = db.relationship('Action', backref='action', lazy='dynamic')
+    actions = db.relationship('Action', backref='action', lazy='dynamic')
 
-    def __init__(self, description, type, monster_image, quest_id, timestamp):
-        if description is not None:
-            description = bleach.clean(description).strip()
-            if description == '':
-                description = None
-
-        if type is not None:
-            type = bleach.clean(type).strip()
-            if type == '':
-                type = None
-
+    def __init__(self, monster_image, quest_id, progress):
         if monster_image is not None:
             monster_image = bleach.clean(monster_image).strip()
             if monster_image == '':
                 monster_image = None
 
-        self.description = description
-        self.type = type
         self.monster_image = monster_image
         self.quest_id = quest_id
-        self.timestamp = timestamp
+        self.progress = progress
 
-    def insert(self):
-        """
-        inserts a new model into a database
-        the model must have a unique id or null id
-        """
-        db.session.add(self)
-        db.session.commit()
 
     def update(self):
         """
         updates a new model into a database
         the model must exist in the database
         """
-        db.session.commit()
-
-    def delete(self):
-        """
-        deletes model from database
-        the model must exist in the database
-        """
-        db.session.delete(self)
         db.session.commit()
 
 
@@ -261,59 +199,22 @@ class Action(db.Model):
     __tablename__ = 'actions'
 
     # Auto-incrementing, unique primary key
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     # encounter_id, foreign_key to the quest table
-    encounter_id = Column(Integer, ForeignKey('encounters.id'), nullable=False)
-    # type
-    type = Column(String(80), nullable=False)
-    # title
-    title = Column(String(80), nullable=False)
+    encounter_id = Column(Integer, ForeignKey('encounters.id'), index=True, nullable=False)
     # description
-    description = Column(String(1000), nullable=False)
-    # time_stamp
-    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    description = Column(String(400), nullable=False)
+    # created_at
+    created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __init__(self, encounter_id, title, description, type, timestamp):
-        if title is not None:
-            title = bleach.clean(title).strip()
-            if title == '':
-                title = None
-
+    def __init__(self, encounter_id, description):
         if description is not None:
             description = bleach.clean(description).strip()
             if description == '':
                 description = None
 
-        if type is not None:
-            type = bleach.clean(type).strip()
-            if type == '':
-                type = None
 
-        self.title = title
         self.description = description
-        self.type = type
         self.encounter_id = encounter_id
-        self.timestamp = timestamp
-
-    def insert(self):
-        """
-        inserts a new model into a database
-        the model must have a unique id or null id
-        """
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        updates a new model into a database
-        the model must exist in the database
-        """
-        db.session.commit()
-
-    def delete(self):
-        """
-        deletes model from database
-        the model must exist in the database
-        """
-        db.session.delete(self)
-        db.session.commit()
--- manage.py
+++ manage.py
@@ -26,34 +26,34 @@ def db_seed():
     # seed anything here we might need
 
     # users
-    ian = User(username='iandouglas', email='ian.douglas@iandouglas.com', timestamp='2021-01-18 02:20:35.356331')
-    shaunda = User(username='shaunda', email='shaunda@example.com', timestamp='2021-01-18 02:20:35.356331')
-    olga = User(username='olga', email='olga@example.com', timestamp='2021-01-18 02:20:35.356331')
-    caleb = User(username='caleb', email='caleb@example.com', timestamp='2021-01-18 02:20:35.356331')
-    curtis = User(username='curtis', email='curtis@example.com', timestamp='2021-01-18 02:20:35.356331')
-    jake = User(username='jake', email='jake@example.com', timestamp='2021-01-18 02:20:35.356331')
-    carson = User(username='carson', email='carson@example.com', timestamp='2021-01-18 02:20:35.356331')
-    george = User(username='george', email='george@example.com', timestamp='2021-01-18 02:20:35.356331')
-
-    # quests
-    catch_troll = Quest(name='Catch a Troll for the Town Wizard', xp=200, encounter_req=1, type='active', timestamp='2021-01-18 02:20:35.356331')
-    root_bandit = Quest(name='Root out the Craghook Bandits', xp=500, encounter_req=3, type='active', timestamp='2021-01-18 02:20:35.356331')
-    slay_dragon = Quest(name='Slay Argoroth the Dragon', xp=1000, encounter_req=5, type='active', timestamp='2021-01-18 02:20:35.356331')
-
-    # user_quests
-    user_quest_1 = UserQuest(quest_id=1, user_id=1, completion_status=False, progress=1, timestamp='2021-01-18 02:20:35.356331')
-    user_quest_2 = UserQuest(quest_id=2, user_id=3, completion_status=False, progress=1, timestamp='2021-01-18 02:20:35.356331')
-    user_quest_3 = UserQuest(quest_id=2, user_id=6, completion_status=False, progress=1, timestamp='2021-01-18 02:20:35.356331')
-
-    # encounters
-    set_troll_bait = Encounter(description='Set Troll Bait', type='active', monster_image='https://images.huffingtonpost.com/2015-02-05-trollersTroll-thumb.jpg', quest_id=1, timestamp='2021-01-18 02:20:35.356331')
-    build_troll_trap = Encounter(description='Build Troll Trap', type='active', monster_image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdIfSV09BWeNuPejZM4txwTFJJKikYV_WMLg&usqp=CAU', quest_id=1, timestamp='2021-01-18 02:20:35.356331')
-    knock_out = Encounter(description='Knock Out the Troll', type='active', monster_image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdIfSV09BWeNuPejZM4txwTFJJKikYV_WMLg&usqp=CAU', quest_id=2, timestamp='2021-01-18 02:20:35.356331')
-
-    # actions
-    leave_steak = Action(type='active', encounter_id=3,  title='Leave a Raw Steak', description='Send a message to a recruiter', timestamp='2021-01-18 02:20:35.356331')
-    hit_troll_with_stick = Action(type='active', encounter_id=1, title='Hit the Troll with a Giant Stick', description='Apply to a Job', timestamp='2021-01-18 02:20:35.356331')
-    hit_troll_with_stick = Action(type='active', encounter_id=1, title='Hit the Troll with a Giant Stick', description='Apply to a Job', timestamp='2021-01-18 02:20:35.356331')
+    ian = User(username='iandouglas', email='ian.douglas@iandouglas.com', xp=0)
+    shaunda = User(username='shaunda', email='shaunda@example.com', xp=300)
+    olga = User(username='olga', email='olga@example.com', xp=400)
+    caleb = User(username='caleb', email='caleb@example.com', xp=150)
+    curtis = User(username='curtis', email='curtis@example.com', xp=2000)
+    jake = User(username='jake', email='jake@example.com', xp=700)
+    carson = User(username='carson', email='carson@example.com', xp=550)
+    george = User(username='george', email='george@example.com', xp=900)
+    #
+    # # quests
+    catch_troll = Quest(name='Catch a Troll for the Town Wizard', xp=200, encounter_req=1, type='active', level=1)
+    root_bandit = Quest(name='Root out the Craghook Bandits', xp=500, encounter_req=3, type='active', level=2)
+    slay_dragon = Quest(name='Slay Argoroth the Dragon', xp=1000, encounter_req=5, type='active', level=3)
+    #
+    # # user_quests
+    user_quest_1 = UserQuest(quest_id=1, user_id=1, completion_status=False, progress=1)
+    user_quest_2 = UserQuest(quest_id=2, user_id=2, completion_status=False, progress=2)
+    user_quest_3 = UserQuest(quest_id=3, user_id=3, completion_status=False, progress=1)
+    #
+    # # encounters
+    set_troll_bait = Encounter(monster_image='https://images.huffingtonpost.com/2015-02-05-trollersTroll-thumb.jpg', quest_id=1, progress=1)
+    build_troll_trap = Encounter(monster_image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdIfSV09BWeNuPejZM4txwTFJJKikYV_WMLg&usqp=CAU', quest_id=1, progress=1)
+    knock_out = Encounter(monster_image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdIfSV09BWeNuPejZM4txwTFJJKikYV_WMLg&usqp=CAU', quest_id=2, progress=1)
+    #
+    # # actions
+    leave_steak = Action(encounter_id=2,  description='Send a message to a recruiter')
+    hit_troll_with_stick = Action(encounter_id=1, description='Apply to a Job')
+    throw_granade = Action(encounter_id=3, description='Schedule a coffee meetup with a target Company')
 
 
     db.session.add(ian)
@@ -68,7 +68,7 @@ def db_seed():
     db.session.add(catch_troll)
     db.session.add(root_bandit)
     db.session.add(slay_dragon)
-
+    #
     db.session.add(user_quest_1)
     db.session.add(user_quest_2)
     db.session.add(user_quest_3)
@@ -76,9 +76,10 @@ def db_seed():
     db.session.add(set_troll_bait)
     db.session.add(build_troll_trap)
     db.session.add(knock_out)
-    # breakpoint()
-    # db.session.add(leave_steak)
-    # db.session.add(hit_troll_with_stick)
+
+    db.session.add(leave_steak)
+    db.session.add(hit_troll_with_stick)
+    db.session.add(throw_granade)
 
     db.session.commit()
     print(f'obj count: {len(db.session.query(User).all())}')
@@ -86,7 +87,7 @@ def db_seed():
     print(f'obj count: {len(db.session.query(UserQuest).all())}')
     print(f'obj count: {len(db.session.query(Encounter).all())}')
     print(f'obj count: {len(db.session.query(Action).all())}')
-
+    #
 
 if __name__ == "__main__":
     manager.run()
