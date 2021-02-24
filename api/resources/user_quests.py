bdb2b5f020d7d1c14494425f03918832d2a72ace
Jake Heft <jakeheft@gmail.com>
2021-02-24 12:02:59 -0700

    Update user quest endpoint to return list of quests
    Co-authored-by: George Soderholm <georgesoderholm@gmail.com>
    Co-authored-by: Shaunda Cunningham <shaunda.cunningham@gmail.com>
    Co-authored-by: Carson Jardine <carsonjardine@gmail.com>

--- api/resources/user_quests.py
+++ api/resources/user_quests.py
@@ -3,6 +3,7 @@ import json
 from flask import jsonify
 
 import bleach
+import jsons
 from flask import request
 from flask_restful import Resource, abort, reqparse, fields, marshal_with
 from sqlalchemy.orm.exc import NoResultFound
@@ -16,13 +17,26 @@ from api.database.models import UserQuest
 #     map(json.dumps, quests)
 
 def _user_quest_payload(quests):
+    quest_objects = {}
+    for progress, quest in quests.items():
+        # breakpoint()
+        quest_objects[f"quest_id_{quest.id}"] = {
+            'id': quest.id,
+            'type': quest.type,
+            'name': quest.name,
+            'xp': quest.xp,
+            'encounter_req': quest.encounter_req,
+            'level': quest.level,
+            'progress': int(progress)
+        }
+    # return {'data': quest_objects}
+    # breakpoint()
     return {
         'data': {
             'id': 'Null',
             'type': 'quests',
             'attributes': {
-                 "quests":
-                 map(json.loads(quests))
+                 "quests": [quest_objects]
             }
         }
     }
@@ -32,18 +46,24 @@ class UserQuestsResource(Resource):
     def get(self, *args, **kwargs):
         user_id = request.view_args['user_id']
         completion_status = request.args['completion_status']
-        quests = []
+        quests = {}
 
         try:
             user = User.query.filter_by(id=user_id).one()
             user_quests = user.user_quests.filter_by(completion_status=completion_status).all()
             for user_quest in user_quests:
+                progress = user_quest.progress
                 quest_id = user_quest.quest_id
-                quests.append(Quest.query.filter_by(id=quest_id).one())
+                # quests.append(Quest.query.filter_by(id=quest_id).one())
+                quests[str(progress)] = Quest.query.filter_by(id=quest_id).one()
+                # breakpoint()
+
+                # breakpoint()
+                # quests.append(Quest.query.filter_by(id=quest_id).one())
 
         except NoResultFound:
             return abort(404)
 
         user_quest_payload = _user_quest_payload(quests)
-        user_quest_payload['success'] = True
+        # user_quest_payload['success'] = True
         return user_quest_payload, 200
