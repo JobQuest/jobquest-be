ef1a4e32d191452d5b273db9cd5aba0161e4a133
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-02-25 19:13:31 -0700

    WIP Create GET function for quest encounters by progress
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>
    Co-authored-by: George Soderholm <62966396+GeorgieGirl24@users.noreply.github.com>
    Co-authored-by: Carson Jardine <65981543+carson-jardine@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- api/resources/quests.py
+++ api/resources/quests.py
@@ -14,13 +14,41 @@ from api.database.models import UserQuest
 from api.database.models import Encounter
 from api.database.models import Action
 
-def _quest_payload(quest):
+def _quest_encounters_payload(encounter):
+    return {
+        'data': {
+            'id': None,
+            'type': 'encounters',
+            'attributes': {
+                'progress': encounter.progress,
+                'monster_image': encounter.monster_image,
+                'actions': _serial_actions(encounter.actions)
+            }
+        }
+    }
+
+def _serial_actions(actions):
+    serial_actions = []
+
+    for action in actions:
+        serial_actions.append({
+            'id': action.id,
+            'description': action.description
+        })
+
+    return serial_actions
 
 class QuestResource(Resource):
     def get(self, *args, **kwargs):
-        quest_id = request.view_args['id']
+        quest_id = request.view_args['quest_id']
         quest = {}
-
         try:
             progress = request.args['progress']
             quest = Quest.query.filter_by(id=quest_id).one()
+            encounter = quest.encounters.filter_by(progress=progress).one()
+            # breakpoint()
+        except Exception:
+            abort()
+
+        quest_encounters_payload = _quest_encounters_payload(encounter)
+        return quest_encounters_payload, 200
